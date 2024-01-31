7c6c2068441b178515f4e797102e7cbc6468f8ae
juan.e-elev <juan.e-elev@PC-5F18VV3>
2024-01-31 10:14:30 +0100

    Add project files.

---
+++ ÖvningXamlDaniel/Page6.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace ButtonColorChangeExample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_MouseEnter(object sender, RoutedEventArgs e)
+        {
+            // Ändra färg när musen går över knappen
+            ((Button)sender).Background = Brushes.LightBlue;
+        }
+
+        private void MyButton_MouseLeave(object sender, RoutedEventArgs e)
+        {
+            // Återställ färg när musen lämnar knappen
+            ((Button)sender).Background = SystemColors.ControlBrush;
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Ändra färg när knappen klickas
+            ((Button)sender).Background = Brushes.Orange;
+
+            // Lägg till din logik här för knappklickhändelsen
+            MessageBox.Show("Knappen klickades!");
+        }
+    }
+}
+
